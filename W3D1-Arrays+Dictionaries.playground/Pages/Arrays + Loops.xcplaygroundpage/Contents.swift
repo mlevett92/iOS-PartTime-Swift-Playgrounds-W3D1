1a1779a40aa039e7ebe8c822aadff2e2983452ec
Michael Levett <ml3v3tt@gmail.com>
2018-03-12 19:00:18 -0700

    W3 L1

--- W3D1-Arrays+Dictionaries.playground/Pages/Arrays + Loops.xcplaygroundpage/Contents.swift
+++ W3D1-Arrays+Dictionaries.playground/Pages/Arrays + Loops.xcplaygroundpage/Contents.swift
@@ -19,7 +19,7 @@ var thirdElement = fruits[2]
 //: ### Challenge 1
 //:
 //: Get the second element in fruits. ie) "Apple"
-
+fruits [1]
 
 
 
@@ -34,7 +34,7 @@ fruits.append("Tomato")
 //:
 //: Insert your favourite fruit at the start of the array. (We haven't covered this so do a quick google search)
 
-
+fruits.insert ("Mango", at: 0)
 
 
 
@@ -49,7 +49,7 @@ var arrayOfBools = [Bool]()
 //:
 //: Create an empty array for storing `Int`s, call it `numbers`.
 //: Add the numbers 10, 20, 60, 30, 40 to the array.
-
+var numbers = [10,20,60,30,40]
 
 
 
@@ -69,14 +69,21 @@ for fruit in fruits {
 //: The printed lines will appear in the console
 
 
-//: ## Bonus Challenge
-//: Write a function that takes in an array of numbers and prints each number in that array
 
-
+func printNum (numbers:[Int]) {
+    
+    for numbers in numbers {
+        
+        print ("\(numbers) is a number")
+        
+    }
+    
+}
 
 
 
 
 
 // Test it by uncommenting the below line of code:
-// printNum(numbers: numbers)
+ printNum(numbers: numbers)
+
